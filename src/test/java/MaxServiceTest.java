2caae64d6e6da5cac9d46666a68a6d1d9600d400
Regina Timirgaleeva <sadikovaregina@mail.ru>
2023-06-21 22:46:45 +0300

    new commit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -11,7 +11,7 @@ public class MaxServiceTest {
         int a = 5;
         int b = 3;
 
-        int expected = a;
+        int expected = 5;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
