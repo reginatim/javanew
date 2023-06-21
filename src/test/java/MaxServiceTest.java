6eb69137c9242992703c6682394f9cff9eee0fa3
Regina Timirgaleeva <sadikovaregina@mail.ru>
2023-06-21 23:18:54 +0300

    new commit 4

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,8 +1,7 @@
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MaxServiceTest {
-
 
     @Test
     public void shouldFindMax() {
@@ -11,7 +10,20 @@ public class MaxServiceTest {
         int a = 5;
         int b = 3;
 
-        int expected = 5;
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
